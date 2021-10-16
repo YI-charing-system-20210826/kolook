54bb546a40079b71d00425e31ac46599d04b1f8f
umemura14 <junya.u0014@outlook.jp>
2021-10-16 13:15:51 +0900

    no message

---
+++ kolook/src/main/java/com/example/kolook/KolookApplication.java
@@ -0,0 +1,13 @@
+package com.example.kolook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KolookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KolookApplication.class, args);
+	}
+
+}
---
+++ kolook/src/test/java/com/example/kolook/KolookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kolook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KolookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
